da233d173246bb53799857d4efe12a536d40f5c8
Rafał Kuźnia <rafal.kuznia@protonmail.com>
2023-11-17 21:33:53 +0100

    9tests

--- tests/test_basic.py
+++ tests/test_basic.py
@@ -3,21 +3,27 @@ import shutil
 from pathlib import Path
 import subprocess
 
+from veccol.types import Spec
+from veccol.runner import Runner
+
 def build_test_app(path: Path) -> None:
     make = shutil.which("make")
     if make is None:
         raise RuntimeError("make not found")
     runner = subprocess.run([make], cwd=path)
 
-
-
 class TestBasicOperation(unittest.TestCase):
     @classmethod
     def setUpClass(cls) -> None:
-        build_test_app(Path("./data"))
+        build_test_app(Path("./tests/data"))
 
     def test_vector_collection(self) -> None:
-        pass
+        with open("./tests/data/spec.yaml") as f:
+            spec = Spec.from_yaml(f.read())
+            runner = Runner(spec)
+            runner.run()
+            print(spec)
+            exit(1)
 
 if __name__ == '__main__':
     unittest.main()
