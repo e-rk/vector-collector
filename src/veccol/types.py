d4be7f43843f6ac2bccf0291b5b625718080545a
Rafał Kuźnia <rafal.kuznia@protonmail.com>
2023-11-17 19:09:25 +0100

    yaml types

---
+++ src/veccol/types.py
@@ -0,0 +1,38 @@
+#
+# Copyright (c) 2023 Rafał Kuźnia <rafal.kuznia@protonmail.com>
+#
+# SPDX-License-Identifier: GPL-3.0-or-later
+#
+
+from dataclasses import dataclass
+from dataclass_wizard import YAMLWizard
+from typing import Any
+
+@dataclass
+class PreConfig:
+    commands: list[str]
+
+@dataclass
+class Value:
+    name: str
+    expr: str
+
+@dataclass
+class Point:
+    locspec: str
+    values: list[Value]
+
+@dataclass
+class Collect:
+    name: str
+    points: list[Point]
+
+@dataclass
+class Config:
+    collect: list[Collect]
+
+@dataclass
+class Spec(YAMLWizard):
+    pre_config: PreConfig
+    config: Config
+    post_config: PreConfig
